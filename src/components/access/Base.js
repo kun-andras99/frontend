0d9b48c54204dea8c7aae9d26f47dcdf09a1ae68
kunandras-99 <kunandras99@gmail.com>
2022-03-25 16:04:31 +0100

    beta

--- src/components/access/Base.js
+++
@@ -1,5 +0,0 @@
-import Container from "../../components/access/Container";
-import Heading from "../../components/access/Heading";
-import Section from "../../components/access/Section";
-import Text from "../../components/access/Section";
-import Li from "../../components/home/clans/Li";
