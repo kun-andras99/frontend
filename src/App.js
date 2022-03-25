ae35b583565ca832536ec67a4dfd5fc5562be763
kunandras-99 <kunandras99@gmail.com>
2022-03-25 14:36:12 +0100

    start

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React velem
+          Learn React velem is
         </a>
       </header>
     </div>
